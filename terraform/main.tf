eb84e09879fcdb76b22b5022c14facbb665c1fa2
AdityaPdev-eng <adityapdev525@gmail.com>
2025-12-17 21:34:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,13 @@ module "eks" {
 module "iam" {
   source = "./modules/iam"
 }
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
